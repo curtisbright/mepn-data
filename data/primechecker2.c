0e758223dd73b1282839d02668f3d9a02b069c07
Curtis Bright <cbright@uwaterloo.ca>
2013-10-15 09:33:09 -0400

    print residue and time

--- data/primechecker2.c
+++ data/primechecker2.c
@@ -1,11 +1,13 @@
-#include <gmp.h>
 #include <stdio.h>
+#include <gmp.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 int main(int argc, char** argv)
-{	char line[100];
-	mpz_t num, nummo, two;
-	mpz_inits(num, nummo, two, NULL);
+{	struct timeval start, end;
+	char line[100];
+	mpz_t num, nummo, two, res;
+	mpz_inits(num, nummo, two, res, NULL);
 	mpz_set_ui(two, 2);
 	FILE* in = fopen("srsieve.out", "r");
 
@@ -19,17 +21,24 @@ int main(int argc, char** argv)
 		mpz_add_ui(num, num, 185);
 		mpz_sub_ui(nummo, num, 1);
 		printf("Testing 4232*23^%d+185...\n", n);
-		mpz_powm(num, two, nummo, num);
-		FILE* out = fopen("prime.out", "w+");
-		if(mpz_cmp_ui(num, 1)==0)
-			fprintf(out, "4232*23^%d+185 is probably prime!\n", n);
+		gettimeofday(&start, 0);
+		mpz_powm(res, two, nummo, num);
+		gettimeofday(&end, 0);
+		double elapsed = (end.tv_sec - start.tv_sec) + ((end.tv_usec - start.tv_usec)/1000000.0F);
+		FILE* out = fopen("prime.out", "a");
+		if(mpz_cmp_ui(res, 1)==0)
+		{	mpz_tdiv_r_2exp(res, res, 64);
+			gmp_fprintf(out, "4232*23^%d+185 is probably prime!  RES: %016ZX  Time: %.2f sec\n", n, res, elapsed);
+		}
 		else
-			fprintf(out, "4232*23^%d+185 is not prime.\n", n);
+		{	mpz_tdiv_r_2exp(res, res, 64);
+			gmp_fprintf(out, "4232*23^%d+185 is not prime.  RES: %016ZX  Time: %.2f sec\n", n, res, elapsed);
+		}
 		fclose(out);
 	}
 
 	fclose(in);
-	mpz_clears(num, nummo, two, NULL);
+	mpz_clears(num, nummo, two, res, NULL);
 
 	return 0;
 }
