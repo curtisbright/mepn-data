5024c7a3e90e5dbae28d2ac3ab62d6998ccda36d
Curtis Bright <cbright@uwaterloo.ca>
2013-11-19 18:33:38 -0500

    add naive generalized order

--- sieving.c
+++ sieving.c
@@ -13,6 +13,13 @@ int naivediscretelog(mpz_t a, int b, mpz_t c, int d, mpz_t p)
 	mpz_t first, cur, pd, base;
 	mpz_inits(first, cur, pd, base, NULL);
 
+	mpz_set_ui(cur, b);
+	mpz_mod(cur, cur, p);
+	if(mpz_sgn(cur)==0)
+	{	mpz_clears(first, cur, pd, NULL);
+		return -1;
+	}
+
 	mpz_set_ui(base, b);
 	mpz_mul_ui(pd, p, d);
 	mpz_add(first, a, c);
@@ -25,7 +32,7 @@ int naivediscretelog(mpz_t a, int b, mpz_t c, int d, mpz_t p)
 		mpz_mod(cur, cur, pd);
 		if(mpz_sgn(cur)==0)
 			break;
-		if(mpz_cmp(cur, first)==0)
+		if(mpz_cmp(cur, first)==0 && n>0)
 		{	n = -1;
 			break;
 		}
@@ -55,16 +62,16 @@ int discretelog(mpz_t a, int b, mpz_t c, int d, mpz_t p)
 	mpz_mod(first, first, pd);
 	mpz_set(cur, first);
 	
-	for(n=1; ; n++)
-	{	mpz_mul_ui(cur, cur, b);
-		mpz_submul_ui(cur, c, b-1);
-		mpz_mod(cur, cur, pd);
-		if(mpz_sgn(cur)==0)
+	for(n=0; ; n++)
+	{	if(mpz_sgn(cur)==0)
 			break;
-		if(mpz_cmp(cur, first)==0)
+		if(mpz_cmp(cur, first)==0 && n>1)
 		{	n = -1;
 			break;
 		}
+		mpz_mul_ui(cur, cur, b);
+		mpz_submul_ui(cur, c, b-1);
+		mpz_mod(cur, cur, pd);
 	}
 
 	mpz_clears(first, cur, pd, NULL);
@@ -94,6 +101,39 @@ int order(int b, mpz_t p)
 	}
 
 	mpz_clears(one, cur, NULL);
+
+	return n;
+}
+
+// Compute smallest n>0 such that (a*b^n+c)/d = (a+c)/d (mod p)
+// returns -1 if no such n exists
+int naivegenorder(mpz_t a, int b, mpz_t c, int d, mpz_t p)
+{	int n;
+	mpz_t first, cur, pd, base;
+	mpz_inits(first, cur, pd, base, NULL);
+
+	mpz_set_ui(cur, b);
+	mpz_mod(cur, cur, p);
+	if(mpz_sgn(cur)==0)
+	{	mpz_clears(first, cur, pd, base, NULL);
+		return -1;
+	}
+
+	mpz_set_ui(base, b);
+	mpz_mul_ui(pd, p, d);
+	mpz_add(first, a, c);
+	mpz_mod(first, first, pd);
+
+	for(n=1; ; n++)
+	{	mpz_powm_ui(cur, base, n, pd);
+		mpz_mul(cur, cur, a);
+		mpz_add(cur, cur, c);
+		mpz_mod(cur, cur, pd);
+		if(mpz_cmp(cur, first)==0)
+			break;
+	}
+
+	mpz_clears(first, cur, pd, base, NULL);
 
 	return n;
 }
@@ -135,10 +175,10 @@ int main(int argc, char** argv)
 	DIR *dp;
 	struct dirent *ep;
 
-	mkdir("work");
+	mkdir("work", S_IRWXU);
 
-	mpz_t p, one;
-	mpz_inits(p, one, NULL);
+	mpz_t p;
+	mpz_inits(p, NULL);
 
 	dp = opendir("./data");
 	if(dp != NULL)
@@ -185,7 +225,8 @@ int main(int argc, char** argv)
 					mpz_mul(temp2, temp2, temp3);
 					mpz_submul_ui(temp2, z, (base-1)/g);
 					mpz_neg(temp3, temp2);
-					if(mpz_sgn(temp2)>=0)
+					// Print family
+					/*if(mpz_sgn(temp2)>=0)
 						if((base-1)/g==1)
 							gmp_printf("%s(%s)^n%s = %Zd*%d^n-%Zd\n", start, middle, end, temp, base, temp2);
 						else
@@ -194,16 +235,19 @@ int main(int argc, char** argv)
 						if((base-1)/g==1)
 							gmp_printf("%s(%s)^n%s = %Zd*%d^n+%Zd\n", start, middle, end, temp, base, temp3);
 						else
-							gmp_printf("%s(%s)^n%s = (%Zd*%d^n+%Zd)/%d\n", start, middle, end, temp, base, temp3, (base-1)/g);
+							gmp_printf("%s(%s)^n%s = (%Zd*%d^n+%Zd)/%d\n", start, middle, end, temp, base, temp3, (base-1)/g);*/
 
 					char exponents[60000];
 					for(int i=0; i<60000; i++)
 						exponents[i] = 1;
 					for(mpz_set_ui(p, 2); mpz_cmp_ui(p, 101)<=0; mpz_nextprime(p, p))
 					{	int n = discretelog(temp, base, temp3, (base-1)/g, p);
+						int n2 = naivediscretelog(temp, base, temp3, (base-1)/g, p);
 						//int ord = order(base, p);
 						int ord = genorder(temp, base, temp3, (base-1)/g, p);
-						//gmp_printf("%Zd %d %d\n", p, n, ord);
+						int ord2 = naivegenorder(temp, base, temp3, (base-1)/g, p);
+						if(n!=n2 || ord!=ord2)
+							gmp_printf("a: %Zd b: %d c: %Zd d: %d p: %Zd | %d %d | %d %d\n", temp, base, temp3, (base-1)/g, p, n, n2, ord, ord2);
 						if(n!=-1 && ord!=-1)
 							for(int j=n; j<60000; j+=ord)
 								exponents[j] = 0;
@@ -211,13 +255,13 @@ int main(int argc, char** argv)
 						//	printf("%d %d\n", n, ord);
 					}
 
-					sprintf(filename, "work/%d-%d.txt", base, count);
+					/*sprintf(filename, "work/%d-%d.txt", base, count);
 					FILE* out = fopen(filename, "w");
 					gmp_fprintf(out, "%Zd %Zd %d\n", temp, temp3, (base-1)/g);
 					for(int i=0; i<60000; i++)
 						if(exponents[i]==1)
 							fprintf(out, "%d\n", i);
-					fclose(out);
+					fclose(out);*/
 
 					mpz_clears(x, y, z, temp, temp2, temp3, temp10, NULL);
 				}
@@ -229,7 +273,7 @@ int main(int argc, char** argv)
 	else
 		perror("Couldn't open the directory");
 
-	mpz_clears(p, one);
+	mpz_clears(p);
 
 	return 0;
 }
